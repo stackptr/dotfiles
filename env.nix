1b4875db65b73a6841ff204919a48ded8fe5dda7
✿ corey (they/them) <corey@x64.co>
2023-07-18 10:07:27 -0700

    Update packages

--- env.nix
+++ env.nix
@@ -1,22 +1,31 @@
 with import <nixpkgs> {}; [
   any-nix-shell
   awscli2
+  btop
   coreutils-full
   /* temporary workaround: https://github.com/docker/compose/issues/10751 */
   docker-compose
   fd
+  ffmpeg_6
   gh
+  htop
   hub
+  jo
   jq
+  mediainfo
   mosh
+  neofetch
   neovim
   pure-prompt
   rcm
   reattach-to-user-namespace
   ripgrep
   shellcheck
+  sox
   tmux
   tree
+  yt-dlp
+  yq
   zsh-history-substring-search
   zsh-syntax-highlighting
 ]
