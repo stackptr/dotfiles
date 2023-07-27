62b1e5c0135bc3046683c010219dc90b4e2fd645
✿ corey <corey@x64.co>
2023-07-27 09:34:20 -0700

    Update rev

--- nix-defexpr/nixpkgs/default.nix
+++ nix-defexpr/nixpkgs/default.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b4f31da7974d82249cd22ff4002419ec46a8f16b.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/05f42aef4e9502a0d0ad79c9debd11d98565cfa7.tar.gz")
